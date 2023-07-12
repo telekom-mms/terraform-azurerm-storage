4192b5ef5170e3bb2cbf25e726f33ce05c0f721e
michaelamattes <41573722+michaelamattes@users.noreply.github.com>
2023-07-12 15:41:38 +0200

    Refactor (#22)
    Refactoring Module

---
+++ examples/min_main.tf
@@ -0,0 +1,26 @@
+module "storage" {
+  source = "registry.terraform.io/telekom-mms/storage/azurerm"
+  storage_account = {
+    stmms = {
+      location            = "westeurope"
+      resource_group_name = "rg-mms-github"
+    }
+  }
+  storage_container = {
+    terraform = {
+      storage_account_name = module.storage.storage_account["stmms"].name
+    }
+  }
+  storage_share = {
+    share-mms = {
+      storage_account_name = module.storage.storage_account["stmms"].name
+      quota                = 5
+    }
+  }
+  storage_share_directory = {
+    files = {
+      storage_account_name = module.storage.storage_account["stmms"].name
+      share_name           = module.storage.storage_share["share-mms"].name
+    }
+  }
+}
--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = ">=3.6"
+      version = ">=3.46.0"
     }
   }
-  required_version = ">=1.1"
+  required_version = ">=1.3"
 }
